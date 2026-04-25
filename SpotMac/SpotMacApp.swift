67516c768b7fb0a9600b229de90bae3c4b6a8c9a
Muhammad Ahmad <ilbigboss21@gmail.com>
2026-04-25 16:54:52 +0800

    feat:add changes

---
+++ SpotMac/SpotMacApp.swift
@@ -0,0 +1,17 @@
+//
+//  SpotMacApp.swift
+//  SpotMac
+//
+//  Created by Muhammad Ahmad on 25/04/2026.
+//
+
+import SwiftUI
+
+@main
+struct SpotMacApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
